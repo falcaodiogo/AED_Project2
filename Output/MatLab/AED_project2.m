3e69f9e059a2ecc2a05141341e2614536a00d40a
zegameiro <jose.mcgameiro@ua.pt>
2023-01-08 19:51:33 +0000

    Almost done

--- Output/MatLab/AED_project2.m
+++ Output/MatLab/AED_project2.m
@@ -6,39 +6,7 @@ five = load("five_letters.txt");
 six = load("six_letters.txt");
 big = load("big_letters.txt");
 
-figure (1)
-histogram(four, 'FaceColor', [0.4660 0.6740 0.1880]);
-xlim([0 18])
-title("Four letters")
-xlabel("Número colisões")
-ylabel("Número de vezes que ocorrem x colisões")
-grid on
-
-figure (2)
-histogram(five, 'FaceColor', [0.4660 0.6740 0.1880]);
-xlim([0 18])
-title("Five letters")
-xlabel("Número colisões")
-ylabel("Número de vezes que ocorrem x colisões")
-grid on
-
-figure (3)
-histogram(six, 'FaceColor', [0.4660 0.6740 0.1880]);
-xlim([0 18])
-title("Six letters")
-xlabel("Número colisões")
-ylabel("Número de vezes que ocorrem x colisões")
-grid on
-
-figure (4)
-histogram(big, 'FaceColor', [0.4660 0.6740 0.1880]);
-xlim([0 18])
-title("Big letters")
-xlabel("Número colisões")
-ylabel("Número de vezes que ocorrem x colisões")
-grid on
-
-figure(5)
+figure(1)
 subplot(2,2,1)
 histogram(four, 'FaceColor', [0.4660 0.6740 0.1880]);
 xlim([0 18])
